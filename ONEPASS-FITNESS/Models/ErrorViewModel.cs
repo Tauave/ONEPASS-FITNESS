c169cc56f49df0b9a44d927d7931a4d4db53185d
ac155895 <ac155895@A27-GM0Z64GA>
2026-06-03 12:18:59 +1200

    Add project files.

---
+++ ONEPASS-FITNESS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ONEPASS_FITNESS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
