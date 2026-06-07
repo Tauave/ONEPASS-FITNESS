10dc0851bf26f420a934c169651633f87acbdf63
Tauave Tofa <ac155895@avcol.school.nz>
2026-06-08 06:58:55 +1200

    identity

--- ONEPASS-FITNESS/Data/DbContext.cs
+++ ONEPASS-FITNESS/Data/DbContext.cs
@@ -1,19 +1,69 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ONEPASS_FITNESS.Models;
 
 namespace ONEPASS_FITNESS.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<PersonalInfo> PersonalInfos { get; set; }
+        public DbSet<Personalinfo> Personalinfos { get; set; }
         public DbSet<Classes> Classes { get; set; }
         public DbSet<ClassBookings> ClassBookings { get; set; }
         public DbSet<Progress> Progress { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Personalinfo>(entity =>
+            {
+                entity.HasKey(p => p.PersonalinfoId);
+
+                entity.HasIndex(p => p.IdentityUserId).IsUnique();
+
+                entity.HasOne<IdentityUser>()
+                    .WithOne()
+                    .HasForeignKey<Personalinfo>(p => p.IdentityUserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Classes>(entity =>
+            {
+                entity.HasKey(c => c.Classid);
+            });
+
+            modelBuilder.Entity<ClassBookings>(entity =>
+            {
+                entity.HasKey(b => b.BookingID);
+
+                entity.HasOne(b => b.Class)
+                    .WithMany(c => c.ClassBookings)
+                    .HasForeignKey(b => b.Classid)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(b => b.Personalinfo)
+                    .WithMany(p => p.ClassBookings)
+                    .HasForeignKey(b => b.Personalinfoid)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Progress>(entity =>
+            {
+                entity.HasKey(p => p.ProgressId);
+
+                entity.Property(p => p.Weight).HasPrecision(6, 2);
+
+                entity.HasOne(p => p.Personalinfo)
+                    .WithMany(pi => pi.Progress)
+                    .HasForeignKey(p => p.Personalinfoid)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
     }
 }
--- ONEPASS-FITNESS/Program.cs
+++ ONEPASS-FITNESS/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using ONEPASS_FITNESS.Data;
+using ONEPASS_FITNESS.Services;
+
 namespace ONEPASS_FITNESS
 {
     public class Program
@@ -9,31 +12,38 @@ namespace ONEPASS_FITNESS
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapStaticAssets();
@@ -41,6 +51,8 @@ namespace ONEPASS_FITNESS
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
+
+            app.MapRazorPages();
 
             app.Run();
         }
